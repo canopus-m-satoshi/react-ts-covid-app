5572972920d0b223aad64fc07788e37206ec3741
Satoshi Matsushima <>
2021-08-23 09:14:47 +0900

    added axios and updated Selector.tsx

--- src/components/Selector.tsx
+++ src/components/Selector.tsx
@@ -1,19 +1,30 @@
 import React from 'react';
+import { useState } from 'react';
 import countriesJson from '../countries.json';
+
+import axios from 'axios';
 
 const Selector = () => {
   const countries = countriesJson;
+  const [country, setCountry] = useState('');
+  const getCountryData = () => {
+    axios
+      .get(`https://api.covid19api.com/country/${country}`)
+      .then((res) => console.log(res));
+  };
 
   return (
     <div>
       <p>Selector</p>
-      <select name="" id="">
+      <select onChange={(e) => setCountry(e.target.value)}>
+        <option>Select A Country</option>
         {countries.map((country, index) => (
           <option value={country.Slug} key={index}>
-            {index} : {country.Slug}
+            {country.Country}
           </option>
         ))}
       </select>
+      <button onClick={getCountryData}>Get Data</button>
     </div>
   );
 };
